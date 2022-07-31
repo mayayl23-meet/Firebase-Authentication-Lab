70ac32f96c5f94b3a6c5dacfc3b851c46479671e
mayayl23-meet <109899357+mayayl23-meet@users.noreply.github.com>
2022-07-31 16:58:58 +0300

    Database lab update

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -20,7 +20,33 @@ def signin():
     return render_template("signin.html")
 
 
-@app.route('/signup', methods=['GET', 'POST'])
+@app.route('/signup', methods=['GET', 'POST'], full_name, username, bio)
+def signup():
+    error = ''
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        userinputs = {'full_name':"", 'username':'', 'bio':""}
+        try:
+            login_session['user'] = auth.create_user_with_email_and_password(email, password)
+            return redirect(url_for('add_tweet'))
+        except:
+
+@app.route('/', methods=['GET', 'POST'])
+def signin():
+    error = ''
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        try:
+            login_session['user'] = auth.sign_in_with_email_and_password(email, password)
+            return redirect(url_for('add_tweet'))
+        except:
+            error = "Authetication Failed"
+    return render_template("signin.html")
+
+
+@app.route('/signup', methods=['GET', 'POST'], full_name, username, bio)
 def signup():
     error = ''
     if request.method == 'POST':
@@ -48,11 +74,13 @@ config = {
   "messagingSenderId": "731672824209",
   "appId": "1:731672824209:web:d81473564536410f67b230",
   "measurementId": "G-Y82MTZYJM6",
-  "databaseURL": ""
+  "databaseURL": "https://fir-lab-aa93d-default-rtdb.europe-west1.firebasedatabase.app/"
 }
 
 firebase = pyrebase.initialize_app(config)
 auth = firebase.auth()
+db = firebase.database()
+
 
 
 if __name__ == '__main__':
